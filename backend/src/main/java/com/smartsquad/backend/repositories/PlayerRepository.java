9d33bb5ba61d93038b606440a601c52ea3b8d643
matanegel <mateg1278@gmail.com>
2026-03-08 18:05:04 +0200

    add entry point to get specific players

--- backend/src/main/java/com/smartsquad/backend/repositories/PlayerRepository.java
+++ backend/src/main/java/com/smartsquad/backend/repositories/PlayerRepository.java
@@ -5,10 +5,16 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 
 @Repository
 public interface PlayerRepository extends JpaRepository<PlayerEntity, Long> {
 
     @Transactional
     public void deleteByName(String name);
+
+
+    List<PlayerEntity> findAllByNameIn(List<String> names);
+
 }
