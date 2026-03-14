9fc2c43820f2e9b633997a068dd97088e3e80b34
matanegel <mateg1278@gmail.com>
2026-03-14 10:09:26 +0200

    add delete all players

--- backend/src/main/java/com/smartsquad/backend/repositories/PlayerRepository.java
+++ backend/src/main/java/com/smartsquad/backend/repositories/PlayerRepository.java
@@ -4,6 +4,8 @@ import com.smartsquad.backend.models.PlayerEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
@@ -13,6 +15,12 @@ public interface PlayerRepository extends JpaRepository<PlayerEntity, Long> {
 
     @Transactional
     void deleteByName(String name);
+
+
+    @Modifying
+    @Transactional
+    @Query(value = "TRUNCATE TABLE players RESTART IDENTITY", nativeQuery = true)
+    void deleteAll();
 
     PlayerEntity findByName(String name);
 
